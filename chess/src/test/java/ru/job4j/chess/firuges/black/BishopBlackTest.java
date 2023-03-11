648cbf0c2de641a6c416c60fbda21b658d9e92bf
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-03-11 10:24:31 +0300

    Набросок

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+
+import static org.assertj.core.api.Assertions.assertThat;
+class BishopBlackTest {
+
+    @Test
+    void position() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        assertThat(bishopBlack.position()).isEqualTo(Cell.C8);
+    }
+    @Test
+    public void way() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        assertThat(bishopBlack.way(Cell.G5)).isEqualTo(new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5});
+    }
+
+    @Test
+    void copy() {
+    }
+}
